4182f8e7f1185e8ddd768221d098f423897c2dd3
MihaiOlah <73793600+MihaiOlah@users.noreply.github.com>
2021-10-07 22:21:15 +0300

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,46 +1,131 @@
-import java.util.List;
-import java.util.ArrayList;
+package ro.uvt.sabloane;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class Book {
-    private String title_;
-    private List<String> content_;
+    private final String title_;
+    private Collection<Author> authors_ = new LinkedList<>();
+    private Collection<Chapter> chapters_ = new LinkedList<>();
 
     public Book(String title)
     {
         title_ = title;
-        content_ = new ArrayList<>();
     }
 
-    public void createNewParagraph(String paragraph)
+    public boolean addAuthor(Author author)
     {
-        content_.add(paragraph);
+        return authors_.add(author);
     }
 
-    public void createNewImage(String image)
+    public boolean removeAuthor(Author author) { return  authors_.remove(author); }
+
+    public boolean addChapter(Chapter chapter)
     {
-        content_.add(image);
+        return chapters_.add(chapter);
     }
 
-    public void createNewTable(String table)
+    public int createChapter(String chapter_name)
     {
-        content_.add(table);
+        chapters_.add(new Chapter(chapter_name));
+        return chapters_.size() - 1;
+    }
+
+    public int removeChapter(String chapter_name)
+    {
+        int index = 0;
+
+        for (Chapter chapter : chapters_)
+        {
+            if (chapter.getChapterName().equalsIgnoreCase(chapter_name.toLowerCase()))
+            {
+                chapters_.remove(chapter);
+                return index;
+            }
+
+            index++;
+        }
+
+        return -1;
+    }
+
+    public Collection<Author> getAuthors() { return Collections.unmodifiableCollection(authors_); }
+
+    public Collection<Chapter> getChapters() { return Collections.unmodifiableCollection(chapters_); }
+
+    public Chapter getChapter(int index)
+    {
+        int tmp = 0;
+
+        for (Chapter chapter : chapters_)
+        {
+            if (tmp == index)
+                return new Chapter(chapter);
+
+            tmp++;
+        }
+
+        return new Chapter("Index out of range, the chapter was not found");
+    }
+
+    public void print()
+    {
+        int counter;
+
+        // Add title
+        System.out.print("Title: " + title_ + '\n');
+        System.out.print("Authors:\n");
+
+        // Add authors
+        counter = 1;
+        for(Author author : authors_)
+        {
+            System.out.print(counter + ". " + author.toString() + '\n');
+            counter++;
+        }
+
+        // Add chapters
+        System.out.print("\nChapters:\n");
+
+        for(Chapter chapter : chapters_)
+        {
+            System.out.print(chapter.toString() + '\n');
+            counter++;
+        }
     }
 
     @Override
     public String toString()
     {
-        StringBuilder string = new StringBuilder();
+        String tmp;
+        StringBuilder content = new StringBuilder();
+        int counter;
 
-        string.append("Title: " + title_ + "\n");
+        // Add title
+        tmp = "Title: " + title_ + '\n';
+        content.append(tmp);
+        content.append("Authors:\n");
 
-        for (String content : content_)
-            string.append(content + '\n');
+        // Add authors
+        counter = 1;
+        for(Author author : authors_)
+        {
+            tmp = counter + ". " + author.toString() + '\n';
+            content.append(tmp);
+            counter++;
+        }
 
-        return string.toString();
-    }
+        // Add chapters
+        tmp = "\nChapters:\n";
+        content.append(tmp);
 
-    public void print()
-    {
-        System.out.println(this);
+        for(Chapter chapter : chapters_)
+        {
+            tmp =  chapter.toString() + '\n';
+            content.append(tmp);
+            counter++;
+        }
+
+        return content.toString();
     }
 }
